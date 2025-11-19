e5885d6ce16418326ad77b8052eb50d69e66a01b
LauraCmay <2025073@student.cct.ie>
2025-11-19 15:08:46 +0000

    2

--- Uber.py
+++ Uber.py
@@ -8,7 +8,7 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
 	'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 DATE_COLUMN = "date/time"
 
-def load_data(nrow):
+def load_data(nrows):
 	data=pd.read_csv(DATA_URL,nrows=nrows)
 	lowercase=lambda x: str(x).lower()
 	data.rename(lowercase, axis="columns", inplace=True)
